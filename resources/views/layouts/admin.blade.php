59bac62d9a367acb42439e6ff3fa45efe4b265fe
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-26 18:07:12 +0200

    testing things

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Laravel') }} | @yield('pageTitle')</title>
 
-    <script src="{{ asset('js/admin.js') }}" defer></script>
+    <script src="{{ asset('js/admin.js') }}" async defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/front.js') }}" defer></script>
+    <script src="{{ asset('js/front.js') }}" async defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
